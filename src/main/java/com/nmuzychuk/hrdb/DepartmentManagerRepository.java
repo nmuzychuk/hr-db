6ff30bd82d43bd187872772d91492e43de6e9d76
Nazarii Muzychuk <muzychuk.n@gmail.com>
2020-04-19 20:45:18 +0300

    Add more repositories

---
+++ src/main/java/com/nmuzychuk/hrdb/DepartmentManagerRepository.java
@@ -0,0 +1,6 @@
+package com.nmuzychuk.hrdb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentManagerRepository extends JpaRepository<DepartmentManager, Long> {
+}
